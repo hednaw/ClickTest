7d6a23e2f6bbc7aa88866e5db22ccafa55301a1f
hednaw <65706445+hednaw@users.noreply.github.com>
2020-10-07 16:02:52 +0200

    Added timer

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 var time = 10;
 var clicks = 0;
-var started = false;
+
+function StartTimer() {
+  if (clicks == 1) {
+    var everysec = setInterval(SubtractTime, 1000);
+  } else {
+    clearInterval(everysec);
+  }
+}
 
 function endTime() {
-  document.getElementById("Timer").innerHTML = clicks / 10 + " CPS";
   document.getElementById("Counter").innerHTML = "";
   document.getElementById("Clicker").remove();
+}
+
+function SubtractTime() {
+  if (time != 0) {
+    time = time - 1;
+    document.getElementById("Timer").innerHTML = "Time: " + String(time);
+  } else if (time == 0) {
+    document.getElementById("Timer").innerHTML = clicks / 10 + " CPS";
+    endTime();
+  }
 }
 
 function Clicked() {
   clicks += 1;
   console.log(clicks);
   document.getElementById("Counter").innerHTML = clicks;
-  setInterval(endTime, 10000);
 }
 
 function Retry() {
